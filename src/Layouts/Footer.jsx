e4e19920b8006ab7826de46e526234d556b77cee
your-github-Gunny2545 <your-github-gun2545eiei@gmail.com>
2022-12-11 11:13:50 +0700

    commit-message

--- src/Layouts/Footer.jsx
+++ src/Layouts/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <>
     <hr />
-    <p style={{textAlign:"center", color:'blue', fontSize: 15 }}>Copyright &copy; 2022: Thanan Panyakodkaew 654020409041, Chanannika65402040902</p>
+    <p style={{textAlign:"center", color:'blue', fontSize: 15 }}>Copyright &copy; NVC Training Center Thanan Panyakodkaew 654020409041, Chanannika65402040902</p>
     </>
   )
 }
